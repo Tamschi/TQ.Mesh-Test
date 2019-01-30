10c05e28d13a4c51cac88bc9bf48790d47117cb2
Tamme Schichler <tammeschichler@googlemail.com>
2019-01-30 12:08:59 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TQ.Mesh_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var nothing = Span<byte>.Empty;
+            foreach (var part in new Mesh.Mesh(nothing))
+            {
+
+            }
+        }
+    }
+}
